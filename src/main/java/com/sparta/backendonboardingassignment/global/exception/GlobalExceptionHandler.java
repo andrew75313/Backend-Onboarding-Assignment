beaa1eedf6e9252b890a8287a747ea402098dbff
Hyeonjin Kim <andrew75313@gmail.com>
2025-01-18 20:50:32 +0900

    fix: exception handler 에 Valid Exception handler 추가 -MethodArgumentNotValidException 클래스의 예외 처리

--- src/main/java/com/sparta/backendonboardingassignment/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/backendonboardingassignment/global/exception/GlobalExceptionHandler.java
@@ -2,9 +2,15 @@ package com.sparta.backendonboardingassignment.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestController
@@ -13,7 +19,20 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationException(MethodArgumentNotValidException ex) {
+
+        List<String> errorMessageList = new ArrayList<>();
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errorMessageList.add(fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest().body(errorMessageList);
+
     }
 
 }
