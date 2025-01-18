0a5bbc5d11b3fe424b785c0c2adedd31dedf7c5f
Hyeonjin Kim <andrew75313@gmail.com>
2025-01-18 14:53:58 +0900

    fix: Username 대소문자 오류 픽스

--- src/main/java/com/sparta/backendonboardingassignment/global/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/backendonboardingassignment/global/security/UserDetailsServiceImpl.java
@@ -18,7 +18,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        User user = userRepository.findByUserName(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다." + username));
 
         return new UserDetailsImpl(user);
